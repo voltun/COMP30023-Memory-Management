746581857a7989e38ef0f4f132bf75ef617ec928
Nicholas Wong <chungw4@student.unimelb.edu.au>
2020-06-05 06:12:45 +0000

    Debugging throughput

--- src/utilities.c
+++ src/utilities.c
@@ -243,8 +243,10 @@ uint32_t *get_throughput(struct datalog_t *log, uint32_t cpu_clock)
         }
     }
 
+    printf("SUM: %"PRIu32"\n", sum);
+    printf("SIZE: %"PRIu32"\n", size);
     //Calculate average
-    avg = (uint32_t) ceil(sum / size);
+    avg = (uint32_t) ceil(((double)sum) / ((double)size));
 
     //Package data for return
     ret_val = malloc(sizeof(uint32_t)*N_THROUGHPUT_METRIC);
